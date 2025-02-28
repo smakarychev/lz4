90fb4940692ad07fc3851c1a0a63505c59bdc937
smakarychev <49116143+smakarychev@users.noreply.github.com>
2025-02-28 18:11:35 +0700

    updated gitignore

--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,17 @@
 project "lz4"
     kind "StaticLib"
-        language "C"
-        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-        
-        files
-        {
-            "src/**.c",
-            "include/**.h",
-        }
+    language "C"
+    warnings "Off"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "src/**.c",
+        "include/**.h",
+    }
 
-        includedirs 
-        {
-            "include/"
-        }
+    includedirs 
+    {
+        "include/"
+    }
